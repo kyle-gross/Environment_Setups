83dd4a3d4aabe51dffb4bd5b3fa742e4b388d4d9
kyle-gross <kyg555@outlook.com>
2021-08-11 16:57:38 +0000

    Add ML setup

---
+++ ml_setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Kyle's ML environment setup script
+
+# Update/upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+# Install pip
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+rm get-pip.py
+
+# Install Packages:
+    # numpy
+pip install --user numpy==1.15
+
+    # scipy
+pip install --user scipy==1.3
+
+    # pycodestyle
+pip install --user pycodestyle==2.5
+
+    # matplotlib
+pip install --user matplotlib==3.0
+pip install --user Pillow
+sudo apt-get -y install python3-tk
